33d171a6dbb341a58d037de7d774e97832e88dfe
Saket Shembekar <saket.shembekar@ithpl.com>
2026-02-12 15:43:56 +0530

    Added js folder

---
+++ Js/3_conversion.js
@@ -0,0 +1,10 @@
+//let score = "23"
+//console.log(typeof score);
+
+//let valueInInt = Number(score)
+//console.log(typeof valueInInt);
+
+let loggedIn = 1
+let booleanLoggedIn = Boolean(loggedIn)
+console.log(typeof booleanLoggedIn)
+
---
+++ Js/datatypes.js
@@ -0,0 +1,6 @@
+"use strict"
+//alert("hello")
+
+console.log(3+3)
+console.log("abc")
+
