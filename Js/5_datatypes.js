f43598ea14d45e6fc87e9521d16c48ff8fc2f6e7
Saket Shembekar <saket.shembekar@ithpl.com>
2026-02-12 17:24:04 +0530

    added datatypes summary

---
+++ Js/5_datatypes.js
@@ -0,0 +1,13 @@
+const arr = ["abc", "def"]
+let myObj = {
+    name: Saket 
+    age: 23
+}
+
+const myFun = function(){
+    console.log("Hello ");
+    
+}
+
+// null gives "Object" as typeof object
+// object => function as typeof object 
