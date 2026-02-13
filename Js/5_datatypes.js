de6635dba3459de5332d65d08a1967db36e5d3af
Saket Shembekar <saket.shembekar@ithpl.com>
2026-02-13 09:52:47 +0530

    added5,6,7

--- Js/5_datatypes.js
+++ Js/5_datatypes.js
@@ -1,13 +1,26 @@
-const arr = ["abc", "def"]
-let myObj = {
-    name: Saket 
-    age: 23
-}
+// const arr = ["abc", "def"]
+// let myObj = {
+//     name: "Saket",
+//     age: 23
+// }
 
-const myFun = function(){
-    console.log("Hello ");
-    
-}
+// const myFun = function () {
+//     console.log("Hello ");
+
+// }
 
 // null gives "Object" as typeof object
-// object => function as typeof object 
+// object => function as typeof object
+
+
+// stack(Primitive, copy is provided)  Heap(Non-primitive, reference of the object is provided)
+
+let userA = {
+    email: "abc@gmail.com"
+}
+
+let userB = userA
+userB.email = "saket@gmail.com"
+
+console.log(userA.email);
+console.log(userB.email);
