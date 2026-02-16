4fd7592a15cd06d6e2b630fb2af268589d3b9e78
Saket Shembekar <shembekarsaket@gmail.com>
2026-02-16 14:09:45 +0530

    removed unwanted files

--- app.js
+++
@@ -1,7 +0,0 @@
-
-const names = require('./names')
-const sayHi = require('./utils')
-
-sayHi (`Susan`)
-    sayHi (names.john)
-    sayHi (names.peter)
--- names.js
+++
@@ -1,5 +0,0 @@
-const secret = `Super secret`
-const john = `John`
-const peter = `Peter`
-
-module.exports = {john, peter}
