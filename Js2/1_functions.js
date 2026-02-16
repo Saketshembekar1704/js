4d93954538df90c258c99a85210fa8f36740690e
Saket Shembekar <shembekarsaket@gmail.com>
2026-02-16 14:11:36 +0530

    addded untracked files

---
+++ Js2/1_functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("S");
+    console.log("a");
+    console.log("k");
+    console.log("e");
+    console.log("t");
+}
+
+//sayMyName()
+
+function addNum(num1, num2){
+    console.log(num1 + num2);
+}
+ addNum(2,3)
---
+++ Js2/3_scope.js
@@ -0,0 +1,8 @@
+let a = 300
+if(true){
+    let a = 30
+    const b = 20
+    console.log("Inner: ", a);
+    
+}
+console.log(a);
