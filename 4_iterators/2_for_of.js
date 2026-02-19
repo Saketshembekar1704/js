e72d12bebaf34f2d199b6aded0983e9b001387b5
Saket Shembekar <shembekarsaket@gmail.com>
2026-02-19 18:07:40 +0530

    map

---
+++ 4_iterators/2_for_of.js
@@ -0,0 +1,20 @@
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+
+}
+
+const greetings = "Hello"
+
+for (const greet of greetings) {
+    // console.log(`Each char is: ${greet}`);
+
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('UK', "United Kingdom")
+map.set('FR', "France")
+//insertion order preserved and no duplicates allowed
+console.log(map);
