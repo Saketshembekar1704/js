42f0ba199d6ae878b09a66b234dcb1bfa327c3cd
Saket Shembekar <shembekarsaket@gmail.com>
2026-02-20 16:35:53 +0530

    updated chaining methods

--- 4_iterators/6_array.js
+++ 4_iterators/6_array.js
@@ -1,3 +1,11 @@
 const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const newNum = num.map((num) => num + 10)
+// const newNum = num.map((num) => num + 10)
+const newNum = num
+    .map((num) => num * 10)
+    .map((num) => num + 1)
+    .filter((num) => num >= 51)
 console.log(newNum);
+
+
+//when chaining happens, next method will
+// recieve value of previous method
