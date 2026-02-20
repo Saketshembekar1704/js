af8f821ed6f378d617cf4d424f61de724964ebc6
Saket Shembekar <shembekarsaket@gmail.com>
2026-02-20 12:42:39 +0530

    printed object using for in loop

---
+++ 4_iterators/3_obj.js
@@ -0,0 +1,17 @@
+const obj = {
+    js : 'javascript',
+    cpp : 'C++',
+    rb : 'Ruby'
+}
+
+for (const key in obj) {
+    // console.log(`${key} shortform is for ${obj[key]}`);
+    
+}
+
+const programming = ["js", "py", "rb", "cpp" ]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    
+}
